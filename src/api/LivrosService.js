6b1aebbaef488580dcebae5e9e50fed66617c2af
pedro <pedrorf.dev@gmail.com>
2025-06-04 16:10:26 -0300

    feat: adding project

---
+++ src/api/LivrosService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3333";
+
+export class LivrosService {
+  static getLivros() {
+    return axios.get(BASE_URL + "/livros");
+  }
+
+  static getLivro(id) {
+    return axios.get(`${BASE_URL}/livros/${id}`);
+  }
+
+  static getLivro(isbn) {
+    return axios.get(`${BASE_URL}/livros/${isbn}`);
+  }
+
+  static createLivro(body) {
+    return axios.post(`${BASE_URL}/livros`, body);
+  }
+
+  static updateLivro(id, body) {
+    return axios.put(`${BASE_URL}/livros/${id}`, body);
+  }
+
+  static deleteLivro(id) {
+    return axios.delete(`${BASE_URL}/livros/${id}`);
+  }
+}
