ccc98476efdba9d144fd8699da4bfbaf472464b5
pedro <pedrorf.dev@gmail.com>
2025-06-09 16:03:26 -0300

    fix: add netlify redirects for SPA

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import netlify from "vite-plugin-netlify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    netlify({
-      redirects: [
-        {
-          from: "/*",
-          to: "/index.html",
-          status: 200,
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
 });
